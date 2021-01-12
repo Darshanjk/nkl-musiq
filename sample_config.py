28c4fb17f9fa06195252b2f980a110b163f796c6
Darshanjk <75562276+Darshanjk@users.noreply.github.com>
2021-01-12 19:01:38 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
 jio_saavn_api = "http://127.0.0.1:5000/result/?query="
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001449437277
+sudo_chat_id = -1001175124374
 owner_id = 1223296516
